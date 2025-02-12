9aa88461c26c4d71c3b23ebbc1791da9a9d74606
mrdestabillo <estabilloelaine7@gmail.com>
2025-02-12 20:44:26 +0800

    Add files via upload

---
+++ DatabaseHandler.java
@@ -0,0 +1,72 @@
+import java.sql.*;
+
+public class DatabaseHandler {
+
+    private static DatabaseHandler handler = null;
+    private static Statement stmt = null;
+    private static PreparedStatement pstatement = null;
+
+    public static DatabaseHandler getInstance() {
+        if (handler == null) {
+            handler = new DatabaseHandler();
+        }
+        return handler;
+    }
+
+    public static Connection getDBConnection()
+    {
+        Connection connection = null ;
+        String dburl = "jdbc:mysql://localhost:3306/mydb";
+        String userName = "root";
+        String password = "@sharlaivynicole25";
+
+        try 
+        {
+            connection = DriverManager.getConnection(dburl, userName, password);
+
+        } catch (Exception e) {
+            
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    public ResultSet execQuery(String query) {
+
+        ResultSet result;
+
+        try {
+            stmt = getDBConnection().createStatement();
+            result = stmt.executeQuery(query);
+        } 
+        catch (SQLException ex) {
+            System.out.println("Exception at execQuery:dataHandler" + ex.getLocalizedMessage());
+            return null;
+        } 
+        finally {
+        }
+        
+        return result;
+    }
+
+    public static boolean validateLogin(String Username, String Password) {
+
+        getInstance();
+        String query = "SELECT * FROM accounts WHERE Username = '" + Username + "' AND Password = '" + Password + "'";
+
+        System.out.println(query);
+
+        ResultSet result = handler.execQuery(query);
+        try {
+            if (result.next()) {
+                return true;
+            }
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+            return false;
+    }
+}
